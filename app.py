1234f7ee68511422c22138b63ddeeadaf466af1c
caojunsheng <caojunsheng@1484266223qq.com>
2020-07-06 20:13:41 +0800

    添加了首页模板

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flaask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello world'
+    return render_template 'home.html'
 
 if __name__ == '__main__':
     app.debug = True
